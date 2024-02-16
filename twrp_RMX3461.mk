b5765462d6548567d1cc518e87655f74d8b79974
Sidharth <157048502+arteryring@users.noreply.github.com>
2024-02-16 16:50:13 +0530

    RMX3461: Initial tree

---
+++ twrp_RMX3461.mk
@@ -0,0 +1,24 @@
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from light device
+$(call inherit-product, device/realme/RMX3461/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_RMX3461
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3461
+PRODUCT_MANUFACTURER := realme
+PRODUCT_DEVICE := RMX3395
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3461-user 13 TYP1A.220905.001 R.12be919_f765 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX3461/RE54BFL1:13/TP1A.220905.001/R.12be919_f765_f764:user/release-keys
